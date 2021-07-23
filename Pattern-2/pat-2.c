868863cc4effceb64421269c4c5fbbf7311295e8
ujjawa-12 <67854090+Ujjawal-kp@users.noreply.github.com>
2021-07-23 16:33:23 +0530

    Star pattern question

---
+++ Pattern-2/pat-2.c
@@ -0,0 +1,24 @@
+/*
+  PATTERN-1
+  *****
+  ****
+  ***
+  **
+  *
+*/
+#include<stdio.h>
+int main(){
+	int i=5,j=5;
+	for(i=1;i<=5;++i){
+		for(j=1;j<=5;++j){
+			if(j<=6-i){
+				printf("*");
+			}
+			else{
+				printf("");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
